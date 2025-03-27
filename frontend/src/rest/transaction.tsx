f1dabef253ae80e9cbd0dfc5d897b347c2fc1c33
Bishesh Agarwal <bisheshagarwal1999@gmail.com>
2025-03-27 23:46:58 +0530

    Minor fixes

--- frontend/src/rest/transaction.tsx
+++ frontend/src/rest/transaction.tsx
@@ -1,12 +1,13 @@
+import { Transact } from "@/types";
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_APP_API_URL;
 
-export function transact(walletId, data) {
+export function transact(walletId = "", data: Transact) {
   return axios.post(`${API_URL}/transact/${walletId}`, data);
 }
 
-export function getTransactions(walletId, limit = 10, skip = 0) {
+export function getTransactions(walletId: string | null, limit = 10, skip = 0) {
   return axios.get(
     `${API_URL}/transactions?walletId=${walletId}&skip=${skip}&limit=${limit}`
   );
